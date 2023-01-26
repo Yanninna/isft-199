14bffd3713c58699995adc3c18789737b4d1299e
Rigoni Yanina <53129565+Yanninna@users.noreply.github.com>
2023-01-26 14:29:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- css bootstrap-->
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap.rtl.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-grid.rtl.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-reboot.rtl.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-utilities.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-utilities.rtl.min.css">
+	
+	<title>ISFT Nro 199</title>
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<?php include ('parts/header.php');?>
+		</div>
+		<div class="jumbotron">
+			<h1 class="display-4">I.S.F.T. N° 199</h1>
+			<p class="lead">Nuestra Historia</p>
+			<hr class="my-4">
+			<p class="text-justify">De acuerdo con lo normado en la Ley de Educación y en la Ley de Educación Superior se establece la jerarquización y revalorización de la Educación Técnica con el fin de mejorar la calidad de los recursos humanos, que optimizarán los procesos en las industrias y en las empresas de servicios. Con esta meta, en el año 2005, fue creada la Extensión Tigre del I.S.F.T. Nº 182, mediante la Resolución 2241/05 de D.G.C.y E. con fecha 24 de mayo de 2005, rubricada por el Sr. Director General de Cultura y Educación.</p>
+			<p class="text-justify">Desde el comienzo en la Escuela de Educación Técnica Nº 1, con un grupo reducido de alumnos, se llegó al presente ciclo lectivo a contar con cinco promociones de Egresados de la Tecnicatura Superior en Turismo y cuatro promociones de Egresados de la carrera de Tecnicatura Superior en Logística. También es importante señalar que en diciembre del 2011 egreso la primera promoción de la Tecnicatura Superior en Seguridad, Higiene y Control Ambiental Industrial y este año además contamos con los tres años de la Tecnicatura Superior en Hotelería. Toda la comunidad educativa de la Extensión, docentes, padres y alumnos, trabajó mancomunadamente y sin descanso para lograr ocupar un lugar reconocido dentro de la región. Es importante señalar que la Extensión del ISFT Nº 182, que en el año 2005, contaba con 25 alumnos, un regente, un bibliotecario y un auxiliar. Hoy presenta otra realidad con 454 alumnos, director, regente, secretaria, jefe de área, bibliotecaria, 4 preceptores, 60 docentes y dos auxiliares.</p>
+			<p class="text-justify">Lo más importante es haber logrado con el esfuerzo de todos ser el primer Instituto Superior de Formación Técnica de Tigre Nº 199 y no depender más de San Miguel.</p>
+			<p class="text-justify">Por Resolución Nº 2981 del 15 de setiembre se crea el I.S.F.T. Nº 199 que funcionara en la EP Nº 15 (Escuela primaria ubicada en Celina Voena 1750)</p>
+			<p class="text-justify">La matrícula de los primeros años al 28 de diciembre del 2011 era de aproximadamente 200 inscriptos. Es importante señalar que la Educación es pública y gratuita. Como se mencionó anteriormente dependemos de la Dirección General de Escuelas de la Provincia de Buenos Aires quien abona los sueldos de los profesores únicamente.</p>
+			<p class="text-justify">Estamos muy contentos por los logros obtenidos pero no dejaremos de trabajar hasta conseguir el objetivo principal NUESTRO PROPIO EDIFICIO.</p>
+			<p></p>
+			<p class="lead">Nuestro Equipo</p>
+			<hr class="my-4">
+			<div class="row">
+			  <div class="col-md-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Arsenio Jose Gonzalez</h3>
+					<p class="card-text">Director</p>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-md-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Andrea Zapata</h3>
+					<p class="card-text">Regente</p>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-md-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Laura Ruiz</h3>
+					<p class="card-text">Secretaria</p>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-md-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Ayala Christian</h3>
+					<p class="card-text">A completar</p>
+				  </div>
+				</div>
+			  </div>
+			</div>
+			<p></p>
+			<p class="lead"> Órganos Institucionales de Participación</p>
+			<hr class="my-4">
+			<div class="row">
+			  <div class="col-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Consejo Académico Institucional (C.A.I.) </h3>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Centro de Estudiantes</h3>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Asociación Cooperadora</h3>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-3">
+				<div class="card">
+				  <div class="card-body">
+					<h3 class="card-title">Biblioteca</h3>
+				  </div>
+				</div>
+			  </div>
+			</div>
+			<p></p>
+</div>
+
+		</div>
+		<div class="footer">
+			<?php include ('parts/footer.php');?>
+		</div>
+
+	</div>
+	<script src="<?php echo $web;?>js/jquery-3.6.0.min.js"></script>
+	<script src="<?php echo $web;?>/js/bootstrap.bundle.min.js"></script>
+	<script src="<?php echo $web;?>/js/bootstrap.esm.min.js"></script>
+	<script src="<?php echo $web;?>/js/bootstrap.min.js"></script>
+</body>
+</html>
