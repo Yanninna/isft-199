14f1a61e176be56d8ee735a65bf2c0fa62d8e0d0
Rigoni Yanina <53129565+Yanninna@users.noreply.github.com>
2023-01-26 14:52:14 -0300

    Add files via upload

---
+++ parts/header.php
@@ -0,0 +1,60 @@
+<?php
+$web = 'http://localhost/isft199/';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap.rtl.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-grid.rtl.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-reboot.rtl.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-utilities.min.css">
+	<link rel="stylesheet" href="<?php echo $web;?>css/bootstrap-utilities.rtl.min.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>ISFT Nro 199</title>
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="<?php echo $web;?>images/logo.jpg" style="height:5em;"></a>
+  
+    
+    <nav class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="<?php echo $web;?>index.php">Institucional</a>
+        </li>
+        <li class="nav-item">            
+       </li>
+       <li class="nav-item">
+            <a class="nav-link active" href="abm-contactos/contacto.php">Contacto</a>
+       </li>
+       <li class="nav-item">
+            <a class="nav-link active" href="abm-noticias/noticias.php">Noticias</a>
+       </li>
+       <li class="nav-item">
+            <a class="nav-link active" href="https://sites.google.com/site/isft199/carreras">Carreras</a>
+       </li>
+       <li class="nav-item">
+            <a class="nav-link active" href="abm-inscripciones/inscripciones.php">Inscripciones</a>
+       </li>
+       
+       <li class="nav-item btn-warning">
+            <a class="nav-link active" href="<?php echo $web;?>login/index.html">Ingresar</a>
+       </li>
+           </nav>
+    </nav>
+    
+</div>
+    <script src="<?php echo $web;?>js/jquery-3.6.0.min.js"></script>
+	<script src="<?php echo $web;?>/js/bootstrap.bundle.min.js"></script>
+	<script src="<?php echo $web;?>/js/bootstrap.esm.min.js"></script>
+	<script src="<?php echo $web;?>/js/bootstrap.min.js"></script>
+</body>
+</html>
